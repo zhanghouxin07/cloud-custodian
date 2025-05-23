066f37e6d4b614b8d331c81587d4c2906d6de505
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2025-05-23 14:29:26 +0800

    huaweicloud - add retry for 429 and network exception (#189)

--- tools/c7n_huaweicloud/c7n_huaweicloud/query.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/query.py
@@ -16,6 +16,7 @@ from c7n_huaweicloud.filters.tms import register_tms_filters
 from c7n_huaweicloud.utils.marker_pagination import MarkerPagination
 
 from huaweicloudsdkcore.exceptions import exceptions
+from huaweicloudsdkcore.retry.backoff_strategy import BackoffStrategies
 
 log = logging.getLogger("custodian.huaweicloud.query")
 
@@ -279,7 +280,29 @@ class ResourceQuery:
         return resources
 
     def _invoke_client_enum(self, client, enum_op, request):
-        return getattr(client, enum_op)(request)
+        _invoker = getattr(client, enum_op)
+        if not enum_op.endswith("_invoker"):
+            return _invoker(request)
+
+        def should_retry(resp, exc):
+            # network connection exception
+            if isinstance(exc, exceptions.ConnectionException):
+                return True
+            # 429 too many requests
+            if isinstance(exc, exceptions.ServerResponseException) and exc.status_code == 429:
+                return True
+
+            return False
+
+        try:
+            return _invoker(request).with_retry(
+                retry_condition=should_retry,
+                max_retries=5,
+                backoff_strategy=BackoffStrategies.EQUAL_JITTER
+            ).invoke()
+        except Exception as e:
+            logging.error(f"Failed after max retries: {str(e)}")
+            raise
 
     def _pagination_ims(self, m, enum_op, path):
         session = local_session(self.session_factory)
