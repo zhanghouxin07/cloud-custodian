6181e38017bc10754af25837e6e510beaf8b8f82
tangtao40huawei <tangtao40@huawei.com>
2025-07-18 16:16:46 +0800

    添加日志流id (#307)
    * modify logs
    
    * add stream id
    
    * bugfix
    
    * some logs
    
    * add log tags
    
    * bugfix
    
    * bugfix
    
    * bugfix
    
    * tiaozhenggeshi
    
    * bugfix
    
    * add logs
    
    * add logs
    
    * make lint

--- tools/c7n_huaweicloud/c7n_huaweicloud/resources/stream.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/resources/stream.py
@@ -4,8 +4,7 @@
 import logging
 import time
 
-from huaweicloudsdklts.v2 import UpdateLogStreamRequest, UpdateLogStreamParams, \
-    ListLogGroupsRequest, ListLogStreamRequest
+from huaweicloudsdklts.v2 import UpdateLogStreamRequest, UpdateLogStreamParams
 
 from c7n.utils import type_schema
 from c7n_huaweicloud.actions.base import HuaweiCloudBaseAction
@@ -23,40 +22,8 @@ class Stream(QueryResourceManager):
         service = 'lts-stream'
         enum_spec = ("list_log_groups", 'log_groups', 'offset')
         id = 'log_group_id'
-        tag = True
+        tags = "tag"
         tag_resource_type = 'lts-stream'
-
-    def get_resources(self, resource_ids):
-        log.info("after listen get all groups")
-        client = self.get_client()
-        streams = []
-        request = ListLogGroupsRequest()
-        stream_request = ListLogStreamRequest()
-        response = client.list_log_groups(request)
-        should_break = False
-        for group in response.log_groups:
-            if group.log_group_name.startswith("functiongraph.log.group"):
-                continue
-            time.sleep(0.22)
-            stream_request.log_group_id = group.log_group_id
-            try:
-                stream_response = client.list_log_stream(stream_request)
-                for stream in stream_response.log_streams:
-                    if stream.log_stream_id == resource_ids[0] and stream.whether_log_storage:
-                        streamDict = {}
-                        streamDict["log_group_id"] = group.log_group_id
-                        streamDict["log_stream_id"] = stream.log_stream_id
-                        streamDict["log_stream_name"] = stream.log_stream_name
-                        streams.append(streamDict)
-                        should_break = True
-                        break
-            except Exception as e:
-                log.error(e)
-                raise
-            if should_break:
-                break
-        log.info("The number of streams to disable storage is " + str(len(streams)))
-        return streams
 
 
 Stream.filter_registry.register('streams-storage-enabled', LtsStreamStorageEnabledFilter)
@@ -70,13 +37,19 @@ class LtsDisableStreamStorage(HuaweiCloudBaseAction):
 
     def perform_action(self, resource):
         time.sleep(0.22)
-        client = self.manager.get_client()
-        request = UpdateLogStreamRequest()
-        request.log_group_id = resource["log_group_id"]
-        request.log_stream_id = resource["log_stream_id"]
-        request.body = UpdateLogStreamParams(
-            whether_log_storage=False
-        )
-        log.info("disable storage: " + resource["log_stream_id"])
-        response = client.update_log_stream(request)
-        return response
+        try:
+            client = self.manager.get_client()
+            request = UpdateLogStreamRequest()
+            request.log_group_id = resource["log_group_id"]
+            request.log_stream_id = resource["log_stream_id"]
+            request.body = UpdateLogStreamParams(
+                whether_log_storage=False
+            )
+            log.info("[actions]-[disable-stream-storage]: The resource:[stream] with"
+                     "id:[{}] modify storage is success".format(resource["log_stream_id"]))
+            response = client.update_log_stream(request)
+            return response
+        except Exception as e:
+            log.error("[actions]-[disable-stream-storage]-The resource:[stream] with id:"
+                      "[{}] modify sotrage failed. cause: {}".format(resource["log_stream_id"], e))
+            raise
