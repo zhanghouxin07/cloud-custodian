432faed3707073bcc50f5647cf3e131f6a6ad70a
zhangshun523 <v_523994191@qq.com>
2025-07-23 15:01:20 +0800

    #cbr raise exception when call sdk api failed. (#321)
    * CBR新增检查是否有cbr vpc终端节点的检查策略
    
    * 优化CBR010规则，去除策略中关于创建存储库相关的参数，改为从已有存储库继承
    
    * 优化CBR010规则，去除策略中关于创建存储库相关的参数，改为从已有存储库继承
    
    * 修改lint“
    
    * 修改lint“
    
    * optimize the scenario where a vault creation confict occurs when an ecs creation event is triggered.
    
    * repair lint
    
    * optimize vault name
    
    * fix review comments
    
    * lint
    
    * CBR01 rule optimization, add default configuration for creating vaults.
    
    * CBR01 rule optimization, add default configuration for creating vaults.
    
    * change schedule per 1h
    
    * #cbr raise exception when call sdk api failed.
    
    * #cbr raise exception when call sdk api failed.
    
    * cbr optimization log records.
    
    * cbr optimization log records.
    
    * cbr optimization log records.

--- tools/c7n_huaweicloud/c7n_huaweicloud/resources/cbr_protectable.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/resources/cbr_protectable.py
@@ -172,8 +172,9 @@ class CbrAssociateServerVault(HuaweiCloudBaseAction):
                               f"{vaults[vault_num]['id']} success.")
             except exceptions.ClientRequestException as e:
                 log.error(f"[actions]-[{self.action_name}] "
-                          f"failed to add resource to {vaults[vault_num]['id']}, "
-                          f"cause request id:{e.request_id}, msg:{e.error_msg}")
+                          f"add resource id:[{server_ids}] to vault id:{vaults[vault_num]['id']}"
+                          f" failed, cause request id:{e.request_id}, msg:{e.error_msg}")
+                raise
             vault_num += 1
         vault_billing = {}
         if len(vaults) > 0:
@@ -309,6 +310,7 @@ class CbrAssociateServerVault(HuaweiCloudBaseAction):
                     log.error(f"[actions]-[{self.action_name}]"
                               f" query policy by vault:{vault_item['id']} failed,"
                               f" cause request id:{e.request_id}, msg:{e.error_msg}")
+                    raise
         if not policy_id:
             # if inherit policy failed, list exists policy
             log.debug(f"[actions]-[{self.action_name}]"
@@ -328,4 +330,5 @@ class CbrAssociateServerVault(HuaweiCloudBaseAction):
                 log.error(f"[actions]-[{self.action_name}]"
                           f" query exist policy list failed,"
                           f" cause request id:{e.request_id}, msg:{e.error_msg}")
+                raise
         return policy_id
