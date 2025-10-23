d9c9475b30d5ef80da27091c70bf40e41924b38c
lvying39726 <308388900@qq.com>
2025-10-23 14:24:32 +0800

    开启轮转之后加一个查询轮转状态动作 (#374)
    * dew custodian代碼
    
    * dew custodian代碼
    
    * dew custodian代碼
    
    * dew custodian代碼
    
    * dew custodian代碼
    
    * dew custodian代碼
    
    * 修正修改
    
    * dew custodian代碼
    
    * dew custodian代碼
    
    * 修改監視
    
    * 修改監視
    
    * 修改監視
    
    * 修改監視
    
    * 修改監視
    
    * 修改監視
    
    * 指定sdkkms版本
    
    * 指定sdkkms版本
    
    * 指定sdkkms版本
    
    * 指定sdkkms版本
    
    * 指定sdkkms版本
    
    * 指定sdkkms版本
    
    * 指定config_resource_support
    
    * 指定config_resource_support
    
    * 指定config_resource_support
    
    * 如果指定密鑰不存在則創建
    
    * 如果指定密鑰不存在則創建
    
    * 如果指定密鑰不存在則創建
    
    * ci
    
    * 加入過濾條件
    
    * 加入過濾條件
    
    * 加入過濾條件
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * ci
    
    * 别名不存在则创建一个时间戳为名的文件再添加别名
    
    * 别名不存在则创建一个时间戳为名的文件再添加别名ci
    
    * 别名不存在则创建一个时间戳为名的文件再添加别名ci
    
    * 仅aes-256和sm4的支持
    
    * 仅aes-256和sm4的支持
    
    * 打印日志,resourceId及成功失败
    
    * 打印日志,resourceId及成功失败
    
    * 打印日志,resourceId及成功失败
    
    * 打印日志,resourceId及成功失败
    
    * 打印日志,enable_key_rotation对于400异常处理
    
    * 打印日志,enable_key_rotation对于400异常处理
    
    * 打印日志,enable_key_rotation对于400异常处理
    
    * 打印日志,enable_key_rotation对于400异常处理
    
    * 打印日志,enable_key_rotation对于400异常处理
    
    * 打印日志,enable_key_rotation对于400异常处理
    
    * 打印日志,enable_key_rotation对于400异常处理
    
    * 打印日志整改
    
    * 打印日志整改
    
    * 打印日志整改
    
    * 打印日志整改
    
    * 打印日志整改
    
    * 打印日志整改
    
    * 打印日志整改
    
    * 打印日志整改
    
    * 打印日志整改
    
    * 打印日志整改
    
    * 打印日志整改
    
    * 打印日志整改
    
    * ut
    
    * ut
    
    * ut
    
    * ut
    
    * ut
    
    * ut
    
    * ut
    
    * ut
    
    * [RR2025072500650]ram共享资源
    
    * [RR2025072500650]ram共享资源
    
    * [RR2025072500650]ram共享资源
    
    * [RR2025072500650]ram共享资源
    
    * [RR2025072500650]ram共享资源
    
    * [RR2025072500650]smn发送消息从event获取
    
    * [RR2025072500650]smn发送消息从event获取
    
    * [RR2025072500650]smn发送消息从event获取
    
    * [RR2025072500650]smn发送消息从event获取
    
    * [RR2025072500650]smn发送消息从event获取
    
    * [RR2025072500650]smn发送消息从event获取
    
    * [RR2025072500650]smn发送消息从event获取
    
    * huaweicloud - FunctionGraph policy only sent event to actions without resources
    
    * huaweicloud - FunctionGraph policy only sent event to actions without resources_add comments
    
    * [RR2025072500650]smn发送消息从event获取
    
    * [RR2025072500650]ci
    
    * [RR2025072500650]ci
    
    * [RR2025072500650]ci
    
    * [RR2025072500650]ci
    
    * [RR2025072500650]ci
    
    * [RR2025072500650]ci
    
    * [RR2025072500650]ci
    
    * [BUG2025081430470]日志debug信息
    
    * [BUG2025081430470]日志debug信息
    
    * [BUG2025090503834]对接tag
    
    * [BUG2025090503834]对接tag
    
    * [BUG2025090503834]对接tag
    
    * [BUG2025090503834]对接tag
    
    * [BUG2025090503834]对接tag
    
    * [BUG2025090503834]对接tag
    
    ---------
    
    Co-authored-by: lvying36729 <lvying15@huawei.com>
    Co-authored-by: yufanxuan <yufanxuan@126.com>

--- tools/c7n_huaweicloud/c7n_huaweicloud/resources/kms.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/resources/kms.py
@@ -4,13 +4,14 @@ import json
 import logging
 import uuid
 import time
+from time import sleep
 
 from huaweicloudsdkkms.v2 import (EnableKeyRotationRequest, OperateKeyRequestBody,
                                   DisableKeyRotationRequest, EnableKeyRequest,
                                   DisableKeyRequest, CreateKeyRequest, CreateKeyRequestBody,
                                   ListAliasesRequest, CreateAliasRequest,
                                   CreateAliasRequestBody, ListKeysRequest, ListKmsByTagsRequest,
-                                  ListKmsByTagsRequestBody)
+                                  ListKmsByTagsRequestBody, ListKeyDetailRequest)
 
 from c7n import exceptions
 from c7n.filters import ValueFilter
@@ -168,12 +169,44 @@ policies:
                 key_id=resource["key_id"],
                 sequence=uuid.uuid4().hex
             )
+
+            requestDetail = ListKeyDetailRequest()
+            requestDetail.body = OperateKeyRequestBody(
+                key_id=resourceId,
+                sequence=uuid.uuid4().hex
+            )
+
+            max_iterations = 5
+            count = 0
+            flag = False
             try:
                 client.enable_key_rotation(request)
                 log.info("[action]-enable_key_rotation the resource:resourceType:KMS "
                          "with resourceId={},"
                          "success"
                          .format(resourceId))
+
+                while count < max_iterations:
+                    response = client.list_key_detail(requestDetail)
+                    keyInfo = response.key_info
+                    if keyInfo.key_rotation_enabled == "true":
+                        flag = True
+                        break
+
+                    log.info(
+                        "[action]-enable_key_rotation the resource:resourceType:KMS with "
+                        "resourceId={} list_key_detail the key is disable,retry {} times"
+                        .format(resourceId, count))
+                    client.enable_key_rotation(request)
+                    sleep(1)
+                    count += 1
+                if not flag:
+                    log.info(
+                        "[action]-enable_key_rotation the resource:resourceType:KMS "
+                        "with resourceId={} "
+                        "is failed, cause list_key_details 5 times,always disabled!"
+                        .format(resourceId))
+
             except Exception as e:
                 if e.status_code == 400:
                     log.info(
